390da9828f6415a1491a9016e225ea817c3cf8d5
Robert Taussig <robert.taussig@gmail.com>
2023-07-19 09:55:47 -0400

    Export Modal

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { ModalsProvider, useModals } from './Modals.context';
 export { ContextMenu } from './components/ContextMenu';
 export { Form } from './components/Form';
 export { default as FullScreenMenu } from './components/FullScreenMenu';
+export { default as Modal } from './components/Modal';
 export { default as PrimaryButton } from './components/PrimaryButton';
 export { default as SecondaryButton } from './components/SecondaryButton';
 export { MyTextField as TextField, MyTextArea as TextArea } from './components/MyTextField';
